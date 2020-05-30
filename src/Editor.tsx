9dd990ce666a9e3501bb87a083a6888f98b34219
tigran25 <tigranvetisyan@gmail.com>
2020-05-30 15:51:05 +0100

    panning working, not zooming to mouse

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,5 +1,5 @@
 import useDocumentTitle from "@rehooks/document-title";
-import React, { useReducer, useRef, useState } from "react";
+import React, { useEffect, useReducer, useRef } from "react";
 import { Image } from "./Drop";
 
 interface State {
@@ -11,6 +11,8 @@ interface State {
     width: number;
     height: number;
   };
+  position: Point;
+  startPosition: Point;
 }
 
 interface Point {
@@ -19,7 +21,15 @@ interface Point {
 }
 
 const reducer = (state: State, action: any) => {
-  const { points, scaleFactor, matrix, viewport } = state;
+  const {
+    points,
+    scaleFactor,
+    matrix,
+    viewport,
+    dragging,
+    startPosition,
+    position,
+  } = state;
 
   switch (action.type) {
     case "click":
@@ -45,28 +55,50 @@ const reducer = (state: State, action: any) => {
       // };
       return state;
     case "move":
-      // if (points.length > 0) {
-      //   return {
-      //     ...state,
-      //     points: [points[0], action.payload],
-      //   };
-      // } else {
-      //   return state;
-      // }
-      return state;
-    case "zoom":
-      // const len = matrix.length;
+      const position2 = { x: action.x, y: action.y };
+      if (dragging) {
+        const dx = position2.x - startPosition.x;
+        const dy = position2.y - startPosition.y;
 
+        const newMatrix = [...matrix];
+        newMatrix[4] += dx;
+        newMatrix[5] += dy;
+
+        return {
+          ...state,
+          position: position2,
+          matrix: newMatrix,
+        };
+      } else {
+        return {
+          ...state,
+          position: position2,
+        };
+      }
+    case "down":
+      return {
+        ...state,
+        startPosition: position,
+        dragging: true,
+      };
+
+    case "up":
+      return {
+        ...state,
+        dragging: false,
+      };
+
+    case "zoom":
       const newMatrix = [...matrix];
 
       for (let i = 0; i < 4; i++) {
-        newMatrix[i] *= action.delta;
+        newMatrix[i] = newMatrix[i] * action.delta;
       }
 
       const [hCenter, vCenter] = [viewport.width / 2, viewport.height / 2];
 
-      newMatrix[4] = (1 - action.delta) * hCenter;
-      newMatrix[5] = (1 - action.delta) * vCenter;
+      newMatrix[4] = (1 - action.delta) * position.x;
+      newMatrix[5] = (1 - action.delta) * position.y;
 
       return {
         ...state,
@@ -80,18 +112,10 @@ const reducer = (state: State, action: any) => {
 const distance = (pt1: Point, pt2: Point): number =>
   Math.hypot(pt2.x - pt1.x, pt2.y - pt1.y);
 
-const extractPoint = (event: React.MouseEvent): Point => ({
-  x: event.clientX,
-  y: event.clientY,
-});
-
-const getTransformToElement = (elem: any) =>
-  elem.getScreenCTM().inverse().multiply(elem.getScreenCTM());
-
 const Editor: React.FC<{ img: Image }> = ({ img }) => {
   useDocumentTitle(`📏 <${img.file.name}>`);
 
-  const [{ points, matrix, scaleFactor, viewport }, dispatch] = useReducer(
+  const [{ points, matrix, position, dragging }, dispatch] = useReducer(
     reducer,
     {
       points: [],
@@ -101,15 +125,36 @@ const Editor: React.FC<{ img: Image }> = ({ img }) => {
         width: 800,
         height: 600,
       },
+      position: {
+        x: 0,
+        y: 0,
+      },
+      startPosition: {
+        x: 0,
+        y: 0,
+      },
     }
   );
   const svgRef = useRef<SVGSVGElement>(null);
   const groupRef = useRef<SVGGElement>(null);
-  const [mousePos, setMousePos] = useState([0, 0]);
+
+  useEffect(() => {
+    const mouseUp = () => dispatch({ type: "up" });
+
+    window.addEventListener("blur", mouseUp);
+    window.addEventListener("mouseup", mouseUp);
+    window.addEventListener("pointerup", mouseUp);
+    return () => {
+      window.removeEventListener("blur", mouseUp);
+      window.removeEventListener("mouseup", mouseUp);
+      window.removeEventListener("pointerup", mouseUp);
+    };
+  });
 
   return (
     <div id="editor">
       <svg
+        overflow="always"
         ref={svgRef}
         id="svg"
         version="1.1"
@@ -119,19 +164,19 @@ const Editor: React.FC<{ img: Image }> = ({ img }) => {
         onClick={(event) =>
           dispatch({
             type: "click",
-            payload: extractPoint(event),
           })
         }
+        onMouseDown={() => dispatch({ type: "down" })}
         onWheel={(e) => {
           if (e.deltaY < 0) {
             dispatch({
               type: "zoom",
-              delta: 1.05,
+              delta: 1.1,
             });
           } else {
             dispatch({
               type: "zoom",
-              delta: 0.95,
+              delta: 0.9,
             });
           }
         }}
@@ -147,7 +192,12 @@ const Editor: React.FC<{ img: Image }> = ({ img }) => {
 
             if (gPt !== null) {
               const { x, y } = pt.matrixTransform(gPt.inverse());
-              setMousePos([x, y]);
+
+              dispatch({
+                type: "move",
+                x,
+                y,
+              });
             }
           }
         }}
@@ -213,7 +263,7 @@ const Editor: React.FC<{ img: Image }> = ({ img }) => {
           </g>
         </g>
       </svg>
-      <div id="info">{JSON.stringify(mousePos.map((p) => p.toFixed(2)))}</div>
+      <div id="info">{JSON.stringify({ dragging, position })}</div>
     </div>
   );
 };
